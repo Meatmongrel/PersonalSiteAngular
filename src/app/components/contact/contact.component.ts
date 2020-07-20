63dc2ebfcf6ebd689cf450269e9b565e774b1286
unknown <squaids449@gmail.com>
2020-07-19 18:25:29 -0600

    V1 of site finished Added projects and contact sections Hosted on firebase Updated styles to fit smaller screens

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  stagger,
+  query,
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  animations: [
+    trigger("grow", [
+      transition(":enter", [
+        query("#email, #phone, #resume", [
+          style({ transform: "scale(0)" }),
+          stagger(400, [
+            animate("1.5s ease", style({ transform: "scale(1)" })),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  @HostBinding("@grow")
+  ngOnInit() {}
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  stagger,
+  query,
+  group,
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  selector: "app-projects",
+  animations: [
+    trigger("slideIn", [
+      transition(":enter", [
+        query(".project-card", [
+          style({ opacity: 0, transform: "translateY(-50vh)" }),
+          stagger(200, [
+            animate("1.2s ease", style({ opacity: 1, transform: "none" })),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
+  templateUrl: "./projects.component.html",
+  styleUrls: ["./projects.component.css"],
 })
 export class ProjectsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
+  @HostBinding("@slideIn")
+  ngOnInit() {}
 }
